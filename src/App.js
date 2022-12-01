69903d9aea84a3af372f49ac3bfa9bf9a2e8abbf
Olga Yasnopolskaya <olga_yasn@hotmail.com>
2022-12-01 22:20:14 +0100

    practiced styled components in css

--- src/App.js
+++ src/App.js
@@ -1,24 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import cocktail from "./assets/whiterussian.jpg";
+import styled from "styled-components";
+import "./App.css";
+
+const Block = styled.div`
+display:flex;
+justify-content:center;
+`
+
+const Card = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background-color: black;
+  width: 45vw;
+  color: white;
+  border-radius: 13px;
+  margin-top: 10vh;
+  font-family: "Courier New", Courier;
+  padding: 5vh 0;
+`;
+
+const Img = styled.img`
+width: 15vw;
+`
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Block>
+    <Card className="card">
+      <h1>White Russian</h1>
+      <Img src={cocktail} alt="white_russian" />
+      <h2>Recette</h2>
+      <ul>
+        <li>4 cl de vodka</li>
+        <li>4 cl de liqueur de café (kahlua, marie brizard)</li>
+        <li>2 cl de lait</li>
+        <li>2 cl de crème fraîche liquide</li>
+      </ul>
+      <h2>Préparation</h2>
+      <ol>
+        <li>Réalisez la recette "White Russian" directement dans le verre.</li>
+        <li>
+          Dans le verre, sur des glaçons, verser les ingrédients. Servir avec un
+          batônnet mélangeur.Pour un peu plus de légèreté, préférez du lait
+          entier à la crème liquide.
+        </li>
+        <li>Servir dans un verre de type "old fashioned".</li>
+        <li>Aucune décoration.</li>
+      </ol>
+    </Card>
+    </Block>
   );
 }
 
